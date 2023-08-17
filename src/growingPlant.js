1e4854c1f5af3605b3bbe566846960ae25b31798
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-17 09:07:01 +0200

    Pseudocode ready

--- src/growingPlant.js
+++ src/growingPlant.js
@@ -1,4 +1,15 @@
-function growingPlant() {
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+
+  // Create dayCounter and currentHeight variable
+
+  // While loop that will increase dayCounter and currentHeight variable based on upSpeed and downSpeed inputes
+
+
+  // If desiredHeight >= currentHeight, break the loop
+
+
+  // return the dayCounter
+
   return 'growingPlant';
 }
 
